8daa96f14718c4f090632af7a6d906e9923599d9
yogesh <yjoon45@gmail.com>
2017-03-25 22:23:35 +0530

    Update packge.json and webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -3,10 +3,10 @@ const { resolve } = require('path');
 
 process.noDeprecation = true;
 
-module.exports = {
+module.exports = env => ({
   context: resolve(__dirname, 'src'),
   entry: './app.js',
-  devtool: 'sourcemap',
+  devtool: env == 'dev' ? 'eval' : 'sourcemap',
   module: {
     rules: [
       {
@@ -19,8 +19,16 @@ module.exports = {
       }
     ]
   },
+  plugins: env == 'dev' ? [] : [
+	  new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: JSON.stringify('production')
+      }
+    }),
+    new webpack.optimize.UglifyJsPlugin()
+  ],
   output: {
     path: resolve(__dirname, 'dist'),
     filename: './js/app.bundle.js'
   }
-};
+});
